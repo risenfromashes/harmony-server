ca286b3c366acb35209050c07e6baf1b40b02412
risenfromashes <ashrafurkhan37@gmail.com>
2022-08-18 10:27:46 +0600

    update main function to read from config.json

--- src/harmony_server.cc
+++ src/harmony_server.cc
@@ -1,58 +1,11 @@
 #include "harmony/http2.h"
-
-#include <atomic>
-#include <cstdint>
-#include <cstdlib>
-#include <iostream>
-#include <thread>
-
 #include "routes.h"
 
 int main(int argc, char **argv) {
-  bool connect_remote_db = true;
-  int threads = std::thread::hardware_concurrency();
-  double timeout = 0.0;
-  // #ifndef NDEBUG
-  const char *port = "5000";
-  // #else
-  //   const char *port = "443";
-  // #endif
-
-  for (int i = 1; i < argc; i++) {
-    std::string_view arg = argv[i];
-    if (arg == "--threads" && (i + 1) < argc) {
-      threads = std::atoi(argv[i + 1]);
-    }
-    if (arg == "--timeout" && (i + 1) < argc) {
-      timeout = std::atoi(argv[i + 1]);
-    }
-
-    if (arg == "--port" && (i + 1) < argc) {
-      port = argv[i + 1];
-    }
-  }
-
-  std::cout << "Server starting with " << threads << " threads and " << timeout
-            << " seconds timeout on port " << port << std::endl;
-
-  hm::Server server({.num_threads = threads,
-                     .port = port,
-                     .dhparam_file = "../harmony-http/certs/dhparam.pem",
-                     .cert_file = "../certs/cert.pem",
-                     .key_file = "../certs/key.pem"});
-
-  server.serve_static_files("../harmony-web/dist/");
-
-  if (connect_remote_db) {
-    server.connect_database(
-        "postgresql://postgres:postgres@localhost:5432/testdb?sslmode=disable");
-  } else {
-    server.connect_database("postgresql:///testdb3");
-  }
-
-  server.set_query_location("../harmony-data/prepared/");
+  hm::Server server(hm::Server::load_config("../config.json"));
 
   server.get("/", [](auto *req, auto *res) { res->send_file("/index.html"); });
+
   server.get("/assets/*", [](auto *req, auto *res) { res->send_file(); });
 
   server.post("/login", login_user);
@@ -63,5 +16,5 @@ int main(int argc, char **argv) {
   server.get("/posts/{user_id}", get_posts);
   server.get("/feed", get_feed);
 
-  server.listen(timeout);
+  server.listen();
 }
